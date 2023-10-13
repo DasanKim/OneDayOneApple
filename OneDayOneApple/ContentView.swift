f49c98dbeb651bfba3775e2ab51fe23c319232b8
dasankim <106504779+DasanKim@users.noreply.github.com>
2023-10-13 19:38:03 +0900

    ✨ Feat: 프로젝트 생성 및 gitignore 수정

---
+++ OneDayOneApple/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OneDayOneApple
+//
+//  Created by Yena on 2023/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneDayOneApple/OneDayOneAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneDayOneAppleApp.swift
+//  OneDayOneApple
+//
+//  Created by Yena on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct OneDayOneAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
