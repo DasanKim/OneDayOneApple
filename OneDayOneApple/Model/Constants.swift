d61b71a4ea7ddd488b839bcd91953355765f6eaf
DasanKim <tempzipp@gmail.com>
2023-10-14 02:01:46 +0900

    ✨Feat: OpenAI Service하는 타입 생성 및 구현

---
+++ OneDayOneApple/Model/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  OneDayOneApple
+//
+//  Created by Dasan on 2023/10/14.
+//
+
+import Foundation
+
+enum Constants {
+    static let OpenAI_API_KEY = ""
+    static let OpenAIBaseURL = "https://api.openai.com"
+    static let OpenAIEndpoint = "/v1/completions"
+}
---
+++ OneDayOneApple/Model/OpenAIService.swift
@@ -0,0 +1,77 @@
+//
+//  OpenAIService.swift
+//  OneDayOneApple
+//
+//  Created by Dasan on 2023/10/14.
+//
+
+import Foundation
+
+final class OpenAIService {
+    private let networkManager = NetworkManager()
+    private let baseURL: String
+    private let endpoint: String
+    
+    init(baseURL: String, endpoint: String) {
+        self.baseURL = baseURL
+        self.endpoint = endpoint
+    }
+}
+
+// request
+// - url: baseURL + endpoint
+// - method: POST
+// - parameters: body
+// - encoder: json
+// - header: Authorization = OpenAI_API_KEY, Content-Type = application/json
+
+// response
+// - id
+// - choices:
+//  - text: Chat GPT text
+//  - (finish_reason)
+// - usage
+//  - promptTokens: 보낼 때 사용한 토큰
+//  - completionTokens: 응답으로 사용된 토큰
+//  - totalTokens: 위의 두 개 다 합친 토큰 개수
+
+extension OpenAIService {
+    private func configureRequest(body: CompletionsBody) -> URLRequest? {
+        guard let url = URL(string: baseURL + endpoint) else {
+            return nil
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("Bearer \(Constants.OpenAI_API_KEY)", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(body) {
+            urlRequest.httpBody = encoded
+        }
+    
+        return urlRequest
+    }
+    
+    func sendMessage(message: String, completion: @escaping (Result<CompletionsResponse, Error>) -> Void) {
+        let body = CompletionsBody(prompt: message)
+        let urlRequest = configureRequest(body: body)
+        
+        networkManager.fetchData(urlRequest: urlRequest) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let response = try JSONDecoder().decode(CompletionsResponse.self, from: data)
+                    completion(.success(response))
+                } catch(let error) {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
