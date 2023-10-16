9caa5a520257cc768c1156ecab95c67b64fad8e8
DasanKim <tempzipp@gmail.com>
2023-10-17 02:40:46 +0900

    ✨Feat: MarkdownType 생성

---
+++ OneDayOneApple/Model/MarkdownType.swift
@@ -0,0 +1,41 @@
+//
+//  MarkdownType.swift
+//  OneDayOneApple
+//
+//  Created by Dasan on 2023/10/16.
+//
+
+enum MarkdownType {
+    case heading1
+    case heading2
+    case heading3
+    case heading4
+    case code
+    case commentStart
+    case commentEnd
+    case newLine
+    case spacing
+    
+    var syntax: String {
+        switch self {
+        case .heading1:
+            return "# "
+        case .heading2:
+            return "## "
+        case .heading3:
+            return "### "
+        case .heading4:
+            return "#### "
+        case .code:
+            return "```"
+        case .commentStart:
+            return "<!--"
+        case .commentEnd:
+            return "-->"
+        case .newLine:
+            return "\n"
+        case .spacing:
+            return " "
+        }
+    }
+}
