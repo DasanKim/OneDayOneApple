186e24b895653d8f565fc755d9e9a126d413a4f8
DasanKim <tempzipp@gmail.com>
2023-10-16 19:00:54 +0900

    ♻️ Refactor: 파일 그룹 수정 및 body에 max_tokens의 기본값 수정

--- OneDayOneApple/Model/OpenAI/DTO/CompletionsBody.swift
+++ OneDayOneApple/Model/OpenAI/DTO/CompletionsBody.swift
@@ -10,15 +10,18 @@ import Foundation
 struct CompletionsBody: Encodable {
     let model: String
     let prompt: String
+    let max_tokens: Int
     let temperature: Float?
     
     init(
         model: String = "gpt-3.5-turbo-instruct",
         prompt: String,
+        max_tokens: Int = 200,
         temperature: Float? = 1.0
     ){
         self.model = model
         self.prompt = prompt
+        self.max_tokens = max_tokens
         self.temperature = temperature
     }
 }
