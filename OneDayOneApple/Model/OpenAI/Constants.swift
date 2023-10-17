5760502f79f3210267865117e66d758c0136f701
DasanKim <tempzipp@gmail.com>
2023-10-17 21:10:33 +0900

    🩹Chore: OpenAI와 관련된 Constants 추가 및 컨벤션 수정

--- OneDayOneApple/Model/OpenAI/Constants.swift
+++ OneDayOneApple/Model/OpenAI/Constants.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum Constants {
-    static let OpenAIBaseURL = "https://api.openai.com"
-    static let OpenAIEndpoint = "/v1/completions"
+    static let openAIBaseURL = "https://api.openai.com"
+    static let openAIEndpoint = "/v1/completions"
+    static let commendTranslateToKorean = "위 문장을 한국어로 번역해줘."
 }
--- OneDayOneApple/Model/OpenAI/OpenAIService.swift
+++ OneDayOneApple/Model/OpenAI/OpenAIService.swift
@@ -16,30 +16,9 @@ final class OpenAIService {
         self.baseURL = baseURL
         self.endpoint = endpoint
     }
-}
-
-extension OpenAIService {
-    private func configureRequest(body: CompletionsBody) -> URLRequest? {
-        guard let url = URL(string: baseURL + endpoint) else {
-            return nil
-        }
-        
-        var urlRequest = URLRequest(url: url)
-        
-        urlRequest.httpMethod = "POST"
-        urlRequest.addValue("Bearer \(Constants.OpenAI_API_KEY)", forHTTPHeaderField: "Authorization")
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let encoder = JSONEncoder()
-        if let encoded = try? encoder.encode(body) {
-            urlRequest.httpBody = encoded
-        }
-    
-        return urlRequest
-    }
     
     func sendMessage(message: String, completion: @escaping (Result<CompletionsResponse, Error>) -> Void) {
-        let body = CompletionsBody(prompt: message + "위 문장을 한국어로 번역해줘.")
+        let body = CompletionsBody(prompt: message + Constants.commendTranslateToKorean)
         let urlRequest = configureRequest(body: body)
         
         networkManager.fetchData(urlRequest: urlRequest) { result in
@@ -58,19 +37,24 @@ extension OpenAIService {
     }
 }
 
-// request
-// - url: baseURL + endpoint
-// - method: POST
-// - parameters: body
-// - encoder: json
-// - header: Authorization = OpenAI_API_KEY, Content-Type = application/json
-
-// response
-// - id
-// - choices:
-//  - text: Chat GPT text
-//  - (finish_reason)
-// - usage
-//  - promptTokens: 보낼 때 사용한 토큰
-//  - completionTokens: 응답으로 사용된 토큰
-//  - totalTokens: 위의 두 개 다 합친 토큰 개수
+extension OpenAIService {
+    private func configureRequest(body: CompletionsBody) -> URLRequest? {
+        guard let url = URL(string: baseURL + endpoint) else {
+            return nil
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("Bearer \(Constants.OpenAI_API_KEY)", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        
+        if let encoded = try? encoder.encode(body) {
+            urlRequest.httpBody = encoded
+        }
+    
+        return urlRequest
+    }
+}
